3ca796c5169de7651d1a309b010ebad298394d4c
SmellyModder <smellysocks543@gmail.com>
2019-07-17 14:20:14 -0400

    Added usage notes to Network Util

--- src/main/java/com/teamabnormals/upgrade_aquatic/api/util/NetworkUtil.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/api/util/NetworkUtil.java
@@ -5,8 +5,16 @@ import com.teamabnormals.upgrade_aquatic.core.UpgradeAquatic;
 
 import net.minecraftforge.fml.network.PacketDistributor;
 
+/**
+ * @author - SmellyModder(Luke Tonon) & ExpensiveKoala
+ * This class holds a list of useful network functions
+ */
 public class NetworkUtil {
 	
+	/**
+	 * @param entityId{int} - The Player Entity's Id
+	 * @param sleepTime{int} - The time since slept to set for the player(Measured in ticks)
+	 */
 	public static void updateCPlayerRestTime(int entityId, int sleepTime) {
 		UpgradeAquatic.CHANNEL.send(PacketDistributor.ALL.noArg(), new MessageCSetRestTime(entityId, sleepTime));
 	}
