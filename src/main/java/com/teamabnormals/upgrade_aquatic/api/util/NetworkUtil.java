2ee1371d29e879d3d3ab98f3a3b80940b9867488
SmellyModder <smellysocks543@gmail.com>
2019-07-19 20:18:23 -0400

    Old way was proper

--- src/main/java/com/teamabnormals/upgrade_aquatic/api/util/NetworkUtil.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/api/util/NetworkUtil.java
@@ -3,8 +3,7 @@ package com.teamabnormals.upgrade_aquatic.api.util;
 import com.teamabnormals.upgrade_aquatic.common.network.MessageCSetRestTime;
 import com.teamabnormals.upgrade_aquatic.core.UpgradeAquatic;
 
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraftforge.fml.network.NetworkDirection;
+import net.minecraftforge.fml.network.PacketDistributor;
 
 /**
  * @author - SmellyModder(Luke Tonon) & ExpensiveKoala
@@ -16,8 +15,8 @@ public class NetworkUtil {
 	 * @param entityId{int} - The Player Entity's Id
 	 * @param sleepTime{int} - The time since slept to set for the player(Measured in ticks)
 	 */
-	public static void updateCPlayerRestTime(int entityId, int sleepTime, ServerPlayerEntity player) {
-		UpgradeAquatic.CHANNEL.sendTo(new MessageCSetRestTime(entityId, sleepTime), player.connection.getNetworkManager(), NetworkDirection.PLAY_TO_CLIENT);
+	public static void updateCPlayerRestTime(int entityId, int sleepTime) {
+		UpgradeAquatic.CHANNEL.send(PacketDistributor.ALL.noArg(), new MessageCSetRestTime(entityId, sleepTime));
 	}
 	
 }
