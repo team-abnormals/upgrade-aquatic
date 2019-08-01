bc2ae65143b2a764a2be6219e4d8e2b4b3066d58
SmellyModder <smellysocks543@gmail.com>
2019-08-01 13:00:24 -0400

    posting the comp

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/registry/other/UACompostables.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/registry/other/UACompostables.java
@@ -1,5 +1,6 @@
 package com.teamabnormals.upgrade_aquatic.core.registry.other;
 
+import com.teamabnormals.upgrade_aquatic.core.registry.UABlocks;
 import com.teamabnormals.upgrade_aquatic.core.registry.UAItems;
 
 import net.minecraft.block.ComposterBlock;
@@ -9,6 +10,8 @@ public class UACompostables {
 	public static void registerCompostables() {
 		ComposterBlock.registerCompostable(0.3F, UAItems.BOILED_PICKERELWEED_BLUE);
 		ComposterBlock.registerCompostable(0.3F, UAItems.BOILED_PICKERELWEED_PURPLE);
+		ComposterBlock.registerCompostable(0.65F, UABlocks.SEAROCKET_WHITE);
+		ComposterBlock.registerCompostable(0.65F, UABlocks.SEAROCKET_PINK);
 	}
 	
 }
