328092bd1e9e92fb283c1c3a0d991bfc336d3269
Voliant <thatvaliantvole@gmail.com>
2019-09-20 06:50:25 +0100

    Made elder eyes not detect armour stands

--- src/main/java/com/teamabnormals/upgrade_aquatic/common/tileentities/TileEntityElderEye.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/common/tileentities/TileEntityElderEye.java
@@ -9,6 +9,7 @@ import com.teamabnormals.upgrade_aquatic.core.registry.UATileEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.ArmorStandEntity;
 import net.minecraft.entity.item.ItemFrameEntity;
 import net.minecraft.entity.item.PaintingEntity;
 import net.minecraft.tileentity.ITickableTileEntity;
@@ -44,7 +45,8 @@ public class TileEntityElderEye extends TileEntity implements ITickableTileEntit
 				for(int i = 0; i < entities.size(); i++) {
 					Entity entity = entities.get(i);
 					
-					if(entity instanceof PaintingEntity || entity instanceof ItemFrameEntity) {
+					if(entity instanceof PaintingEntity || entity instanceof ItemFrameEntity || entity instanceof ArmorStandEntity)
+					{
 						entityCount--;
 					}
 					
