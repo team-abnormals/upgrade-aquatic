87121eb76dd8ace0805936b6b89bad9d113e0991
SmellyModder <smellysocks543@gmail.com>
2019-12-01 11:57:40 -0500

    Potion Functions for flare

--- src/main/java/com/teamabnormals/upgrade_aquatic/common/effects/EffectInsomnia.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/common/effects/EffectInsomnia.java
@@ -1,11 +1,16 @@
 package com.teamabnormals.upgrade_aquatic.common.effects;
 
+import com.teamabnormals.upgrade_aquatic.common.entities.EntityFlare;
+import com.teamabnormals.upgrade_aquatic.core.registry.UAEntities;
+
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.PhantomEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.EffectType;
 import net.minecraft.potion.InstantEffect;
 import net.minecraft.stats.StatisticsManager;
 import net.minecraft.stats.Stats;
+import net.minecraft.util.DamageSource;
 
 public class EffectInsomnia extends InstantEffect {
 	
@@ -14,11 +19,18 @@ public class EffectInsomnia extends InstantEffect {
     }
     
     @Override
-    public void performEffect(LivingEntity entityLivingBase, int amplifier) {
-    	if(entityLivingBase instanceof ServerPlayerEntity) {
-    		ServerPlayerEntity playerMP = (ServerPlayerEntity) entityLivingBase;
+    public void performEffect(LivingEntity entity, int amplifier) {
+    	if(entity instanceof ServerPlayerEntity) {
+    		ServerPlayerEntity playerMP = (ServerPlayerEntity) entity;
     		StatisticsManager statisticsManager = playerMP.getStats();
     		statisticsManager.increment(playerMP, Stats.CUSTOM.get(Stats.TIME_SINCE_REST), (24000 * (amplifier + 1)));
+    	} else if(entity instanceof PhantomEntity) {
+    		EntityFlare flare = UAEntities.FLARE.create(entity.world);
+    		flare.setLocationAndAngles(entity.posX, entity.posY, entity.posZ, entity.rotationYaw, entity.rotationPitch);
+    		entity.world.addEntity(flare);
+    		entity.remove(true);
+    	} else if(entity instanceof EntityFlare) {
+    		entity.attackEntityFrom(DamageSource.MAGIC, Float.MAX_VALUE);
     	}
     }
     
--- src/main/java/com/teamabnormals/upgrade_aquatic/common/effects/EffectRestfulness.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/common/effects/EffectRestfulness.java
@@ -1,5 +1,8 @@
 package com.teamabnormals.upgrade_aquatic.common.effects;
 
+import com.teamabnormals.upgrade_aquatic.common.entities.EntityFlare;
+
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.PhantomEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
@@ -16,13 +19,18 @@ public class EffectRestfulness extends InstantEffect {
     }
     
     @Override
-    public void performEffect(LivingEntity entityLivingBase, int amplifier) {
-    	if (entityLivingBase instanceof ServerPlayerEntity) {
-    		ServerPlayerEntity playerMP = (ServerPlayerEntity) entityLivingBase;
+    public void performEffect(LivingEntity entity, int amplifier) {
+    	if (entity instanceof ServerPlayerEntity) {
+    		ServerPlayerEntity playerMP = (ServerPlayerEntity) entity;
             StatisticsManager statisticsManager = playerMP.getStats();
             statisticsManager.increment(playerMP, Stats.CUSTOM.get(Stats.TIME_SINCE_REST), -(24000 * (amplifier + 1)));
-        } else if(entityLivingBase instanceof PhantomEntity) {
-        	entityLivingBase.attackEntityFrom(DamageSource.MAGIC, Float.MAX_VALUE);
+        } else if(entity instanceof PhantomEntity) {
+        	entity.attackEntityFrom(DamageSource.MAGIC, Float.MAX_VALUE);
+        } else if(entity instanceof EntityFlare) {
+        	PhantomEntity phantom = EntityType.PHANTOM.create(entity.world);
+    		phantom.setLocationAndAngles(entity.posX, entity.posY, entity.posZ, entity.rotationYaw, entity.rotationPitch);
+    		entity.world.addEntity(phantom);
+    		entity.remove(true);
         }
     }
     
