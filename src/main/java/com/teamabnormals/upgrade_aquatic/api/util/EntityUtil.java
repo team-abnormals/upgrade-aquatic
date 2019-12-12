948ef51295ff7a217a4945a7e01f4085652b1522
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2019-12-11 20:23:53 -0600

    Added EmeraldsForItemsTrade and ItemsForEmeraldsAndItemsTrade
    will add new trades soon

--- src/main/java/com/teamabnormals/upgrade_aquatic/api/util/EntityUtil.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/api/util/EntityUtil.java
@@ -57,4 +57,72 @@ public class EntityUtil {
 	    }
 	}
 	
+	static class EmeraldsForItemsTrade implements ITrade {
+		private final ItemStack itemstack;
+		private final int stackSize;
+		private final int recievedSize;
+		private final int maxUses;
+		private final int givenExp;
+		private final float priceMultiplier;
+		
+	    public EmeraldsForItemsTrade(Block block, int stackSize, int recievedSize, int maxUses, int givenExp) {
+	    	this(new ItemStack(block), stackSize, recievedSize, maxUses, givenExp);
+	    }
+
+	    public EmeraldsForItemsTrade(Item item, int stackSize, int recievedSize, int givenExp) {
+	    	this(new ItemStack(item), stackSize, recievedSize, 12, givenExp);
+	    }
+
+	    public EmeraldsForItemsTrade(Item item, int stackSize, int recievedSize, int maxUses, int givenExp) {
+	    	this(new ItemStack(item), stackSize, recievedSize, maxUses, givenExp);
+	    }
+
+	    public EmeraldsForItemsTrade(ItemStack stack, int stackSize, int recievedSize, int maxUses, int givenExp) {
+	    	this(stack, stackSize, recievedSize, maxUses, givenExp, 0.05F);
+	    }
+	    
+		public EmeraldsForItemsTrade(ItemStack stack, int stackSize, int recievedSize, int maxUses, int givenExp, float priceMultiplier) {
+		   	this.itemstack = stack;
+		   	this.stackSize = stackSize;
+		   	this.recievedSize = recievedSize;
+		   	this.maxUses = maxUses;
+		   	this.givenExp = givenExp;
+		   	this.priceMultiplier = priceMultiplier;
+		}
+
+		public MerchantOffer getOffer(Entity p_221182_1_, Random p_221182_2_) {
+		  	return new MerchantOffer(new ItemStack(this.itemstack.getItem(), this.stackSize), new ItemStack(Items.EMERALD, this.recievedSize), this.maxUses, this.givenExp, this.priceMultiplier);
+		}
+	}
+	
+	static class ItemsForEmeraldsAndItemsTrade implements ITrade {
+	    private final ItemStack buyingItem;
+	    private final int buyingItemCount;
+	    private final int emeraldCount;
+	    private final ItemStack sellingItem;
+	    private final int sellingItemCount;
+		private final int maxUses;
+		private final int givenExp;
+		private final float priceMultiplier;
+	    
+	    public ItemsForEmeraldsAndItemsTrade(Item item, int stackSize, Item item2, int recievedSize, int maxUses, int givenExp) {
+	       this(new ItemStack(item), stackSize, 1, new ItemStack(item2), recievedSize, maxUses, givenExp, 0.05F);
+	    }
+
+	    public ItemsForEmeraldsAndItemsTrade(ItemStack stack, int stackSize, int emeraldCount, ItemStack recieved, int recievedSize, int maxUses, int givenExp, float priceMultiplier) {
+	       this.buyingItem = stack;
+	       this.buyingItemCount = stackSize;
+	       this.emeraldCount = emeraldCount;
+	       this.sellingItem = recieved;
+	       this.sellingItemCount = recievedSize;
+	       this.maxUses = maxUses;
+	       this.givenExp = givenExp;
+	       this.priceMultiplier = 0.05F;
+	    }
+		
+	    public MerchantOffer getOffer(Entity p_221182_1_, Random p_221182_2_) {
+	    	return new MerchantOffer(new ItemStack(Items.EMERALD, this.emeraldCount), new ItemStack(this.buyingItem.getItem(), this.buyingItemCount), new ItemStack(this.sellingItem.getItem(), this.sellingItemCount), this.maxUses, this.givenExp, this.priceMultiplier);
+	    }
+	}
 }
+
