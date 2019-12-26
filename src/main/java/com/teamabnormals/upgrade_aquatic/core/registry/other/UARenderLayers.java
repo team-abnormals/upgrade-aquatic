b5ab65384ae59a6216b0bb2e11fadd7f8d133875
SmellyModder <smellysocks543@gmail.com>
2019-12-26 12:49:29 -0500

    Added 1.15 render layer register in preperation for port

---
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/registry/other/UARenderLayers.java
@@ -0,0 +1,17 @@
+package com.teamabnormals.upgrade_aquatic.core.registry.other;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class UARenderLayers {
+	
+	//private final RenderType TRANSPARENT = RenderType.cutoutMipped();
+	//private final RenderType CUTOUT = RenderType.cutout();
+	//private final RenderType TRANSLUSCENT = RenderType.translucent();
+	
+	public static void setBlockRenderLayers() {
+		//RenderTypeLookup.setRenderLayer(UABlocks.BEACHGRASS, CUTOUT);
+	}
+	
+}
