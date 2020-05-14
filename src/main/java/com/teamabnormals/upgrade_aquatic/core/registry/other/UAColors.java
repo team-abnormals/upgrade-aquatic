d43ce3f3eac57074d8a3084d2f533ef5601cb165
SmellyModder <smellysocks543@gmail.com>
2020-05-14 16:10:33 -0400

    Fixed possible NPEs with Block Colors on loading, closes #133

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/registry/other/UAColors.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/registry/other/UAColors.java
@@ -1,23 +1,55 @@
 package com.teamabnormals.upgrade_aquatic.core.registry.other;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.teamabnormals.upgrade_aquatic.core.registry.UABlocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraft.client.renderer.color.IBlockColor;
+import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.world.FoliageColors;
 import net.minecraft.world.biome.BiomeColors;
+import net.minecraftforge.fml.RegistryObject;
 
-public class UAColors
-{
+public class UAColors {
+	
 	public static void registerBlockColors() {
-        BlockColors blockColors = Minecraft.getInstance().getBlockColors();
-        blockColors.register((x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), UABlocks.RIVER_LEAVES.get());
-        blockColors.register((x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), UABlocks.RIVER_LEAF_CARPET.get());
-        blockColors.register((x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), UABlocks.MULBERRY_VINE.get());
+		BlockColors blockColors = Minecraft.getInstance().getBlockColors();
+		registerBlockColor(blockColors, (x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), Arrays.asList(UABlocks.RIVER_LEAVES));
+		registerBlockColor(blockColors, (x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), Arrays.asList(UABlocks.RIVER_LEAF_CARPET));
+		registerBlockColor(blockColors, (x, world, pos, u) -> world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.get(0.5D, 1.0D), Arrays.asList(UABlocks.MULBERRY_VINE));
         
-        ItemColors itemColors = Minecraft.getInstance().getItemColors();
-        itemColors.register((color, items) -> FoliageColors.get(0.5D, 1.0D), UABlocks.RIVER_LEAVES.get());
-        itemColors.register((color, items) -> FoliageColors.get(0.5D, 1.0D), UABlocks.RIVER_LEAF_CARPET.get());
-    }
+		ItemColors itemColors = Minecraft.getInstance().getItemColors();
+		registerBlockItemColor(itemColors, (color, items) -> FoliageColors.get(0.5D, 1.0D), Arrays.asList(UABlocks.RIVER_LEAVES));
+		registerBlockItemColor(itemColors, (color, items) -> FoliageColors.get(0.5D, 1.0D), Arrays.asList(UABlocks.RIVER_LEAF_CARPET));
+	}
+	
+	private static void registerBlockColor(BlockColors blockColors, IBlockColor color, List<RegistryObject<Block>> blocksIn) {
+		List<RegistryObject<Block>> registryObjects = blocksIn;
+		registryObjects.removeIf(block -> !block.isPresent());
+		if(registryObjects.size() > 0) {
+			Block[] blocks = new Block[registryObjects.size()];
+			for(int i = 0; i < registryObjects.size(); i++) {
+				blocks[i] = registryObjects.get(i).get();
+			}
+			blockColors.register(color, blocks);
+		}
+	}
+	
+	private static void registerBlockItemColor(ItemColors blockColors, IItemColor color, List<RegistryObject<Block>> blocksIn) {
+		List<RegistryObject<Block>> registryObjects = blocksIn;
+		registryObjects.removeIf(block -> !block.isPresent());
+		if(registryObjects.size() > 0) {
+			Block[] blocks = new Block[registryObjects.size()];
+			for(int i = 0; i < registryObjects.size(); i++) {
+				blocks[i] = registryObjects.get(i).get();
+			}
+			blockColors.register(color, blocks);
+		}
+	}
+	
 }
