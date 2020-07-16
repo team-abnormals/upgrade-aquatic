803f806a6664cacfb20de07dfe114bff548c3ee2
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-07-15 23:50:39 -0500

    Fixed Sonar Wave EntityClassification

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/registry/UAEntities.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/registry/UAEntities.java
@@ -29,7 +29,7 @@ import net.minecraftforge.fml.common.Mod;
 public class UAEntities {
 	public static final UARegistryHelper HELPER = UpgradeAquatic.REGISTRY_HELPER;
 	
-	public static final RegistryObject<EntityType<EntitySonarWave>> SONAR_WAVE            = HELPER.createEntity("sonar_wave", EntitySonarWave::new, EntitySonarWave::new, EntityClassification.AMBIENT, 1.0F, 1.0F);
+	public static final RegistryObject<EntityType<EntitySonarWave>> SONAR_WAVE            = HELPER.createEntity("sonar_wave", EntitySonarWave::new, EntitySonarWave::new, EntityClassification.MISC, 1.0F, 1.0F);
 	public static final RegistryObject<EntityType<EntityNautilus>> NAUTILUS               = HELPER.createLivingEntity("nautilus", EntityNautilus::new, EntityClassification.CREATURE, 0.5F, 0.5F);
 	public static final RegistryObject<EntityType<EntityPike>> PIKE                       = HELPER.createLivingEntity("pike", EntityPike::new, EntityClassification.CREATURE, 0.7F, 0.4F);
 	public static final RegistryObject<EntityType<EntityLionfish>> LIONFISH               = HELPER.createLivingEntity("lionfish", EntityLionfish::new, EntityClassification.CREATURE, 0.6F, 0.5F);
@@ -37,7 +37,7 @@ public class UAEntities {
 	public static final RegistryObject<EntityType<EntityGreatThrasher>> GREAT_THRASHER    = HELPER.createLivingEntity("great_thrasher", EntityGreatThrasher::new, EntityClassification.MONSTER, 2.8F, 1.575F);
 	public static final RegistryObject<EntityType<EntityFlare>> FLARE                     = HELPER.createLivingEntity("flare", EntityFlare::new, EntityClassification.MONSTER, 0.9F, 0.5F);
 	public static final RegistryObject<EntityType<EntityUlulu>> ULULU					  = HELPER.createLivingEntity("ululu", EntityUlulu::new, EntityClassification.MONSTER, 2.04F, 2.04F);
-	public static final RegistryObject<EntityType<EntityGoose>> GOOSE 				  = HELPER.createLivingEntity("goose", EntityGoose::new, EntityClassification.CREATURE, 0.5F, 0.9F);
+	public static final RegistryObject<EntityType<EntityGoose>> GOOSE 				  	  = HELPER.createLivingEntity("goose", EntityGoose::new, EntityClassification.CREATURE, 0.5F, 0.9F);
 	
 	public static final RegistryObject<EntityType<EntityBoxJellyfish>> BOX_JELLYFISH             = HELPER.createLivingEntity("box_jellyfish", EntityBoxJellyfish::new, EntityClassification.WATER_CREATURE, 0.75F, 0.625F);
 	public static final RegistryObject<EntityType<EntityCassiopeaJellyfish>> CASSIOPEA_JELLYFISH = HELPER.createLivingEntity("cassiopea_jellyfish", EntityCassiopeaJellyfish::new, EntityClassification.WATER_CREATURE, 0.6875F, 0.25F);
