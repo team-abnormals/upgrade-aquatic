2981fcc011374b77099a532fe5af898b49b7098e
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-07-15 22:16:13 -0500

    Fixed River Tree

--- src/main/java/com/teamabnormals/upgrade_aquatic/common/blocks/trees/RiverTree.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/common/blocks/trees/RiverTree.java
@@ -8,12 +8,14 @@ import com.teamabnormals.upgrade_aquatic.common.world.gen.UAFeatures;
 import com.teamabnormals.upgrade_aquatic.common.world.gen.feature.FeatureRiverTree;
 
 import net.minecraft.block.trees.Tree;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 public class RiverTree extends Tree {
 	
 	@Nullable
-	protected ConfiguredFeature<TreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean p_225546_2_) {
+	@Override
+	protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean p_225546_2_) {
 		return UAFeatures.RIVER_TREE.get().withConfiguration(FeatureRiverTree.RIVER_TREE_CONFIG);
 	}
 	
