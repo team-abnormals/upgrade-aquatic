9bf12a596c59bcf23828157e1ec1180e0472a552
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-08-08 12:55:32 -0700

    converted spaces to tabs for consistency with other classes

--- src/main/java/com/minecraftabnormals/upgrade_aquatic/core/mixin/DrownedRendererMixin.java
+++ src/main/java/com/minecraftabnormals/upgrade_aquatic/core/mixin/DrownedRendererMixin.java
@@ -15,23 +15,23 @@ import net.minecraft.util.math.vector.Vector3f;
 
 @Mixin(DrownedRenderer.class)
 public class DrownedRendererMixin extends AbstractZombieRenderer<DrownedEntity, DrownedModel<DrownedEntity>> {
-    private DrownedRendererMixin(EntityRendererManager p_i50974_1_, DrownedModel<DrownedEntity> p_i50974_2_, DrownedModel<DrownedEntity> p_i50974_3_, DrownedModel<DrownedEntity> p_i50974_4_) {
-        super(p_i50974_1_, p_i50974_2_, p_i50974_3_, p_i50974_4_);
-    }
+	private DrownedRendererMixin(EntityRendererManager p_i50974_1_, DrownedModel<DrownedEntity> p_i50974_2_, DrownedModel<DrownedEntity> p_i50974_3_, DrownedModel<DrownedEntity> p_i50974_4_) {
+		super(p_i50974_1_, p_i50974_2_, p_i50974_3_, p_i50974_4_);
+	}
 	
     @Overwrite
     protected void applyRotations(DrownedEntity entityLiving, MatrixStack matrixStackIn, float ageInTicks, float rotationYaw, float partialTicks) {
-        float swimAnimationTicks = entityLiving.getSwimAnimation(partialTicks);
-        if (entityLiving.isInWater()) {
-            super.applyRotations(entityLiving, matrixStackIn, ageInTicks, rotationYaw, partialTicks);
-            float rotationPitchChange = entityLiving.isInWater() ? -90.0F - entityLiving.rotationPitch : -90.0F;
-            float rotationModifier = MathHelper.lerp(swimAnimationTicks, 0.0F, rotationPitchChange);
-            matrixStackIn.rotate(Vector3f.XP.rotationDegrees(rotationModifier));
-            if (entityLiving.isActualySwimming()) {
-                matrixStackIn.translate(0.0D, -1.0D, (double)0.3F);
-            }
-           } else {
-            super.applyRotations(entityLiving, matrixStackIn, ageInTicks, rotationYaw, partialTicks);
-           }
+    	float swimAnimationTicks = entityLiving.getSwimAnimation(partialTicks);
+    	if (entityLiving.isInWater()) {
+    		super.applyRotations(entityLiving, matrixStackIn, ageInTicks, rotationYaw, partialTicks);
+    		float rotationPitchChange = entityLiving.isInWater() ? -90.0F - entityLiving.rotationPitch : -90.0F;
+    		float rotationModifier = MathHelper.lerp(swimAnimationTicks, 0.0F, rotationPitchChange);
+    		matrixStackIn.rotate(Vector3f.XP.rotationDegrees(rotationModifier));
+    		if (entityLiving.isActualySwimming()) {
+    			matrixStackIn.translate(0.0D, -1.0D, (double)0.3F);
+    		}
+    	} else {
+    		super.applyRotations(entityLiving, matrixStackIn, ageInTicks, rotationYaw, partialTicks);
+    	}
     }
 }
