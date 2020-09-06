93d7360c48d249303d475cf073815651ad1edf93
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-09-06 17:53:26 -0500

    Fixed some sketchy kelp stuff

--- src/main/java/com/minecraftabnormals/upgrade_aquatic/common/blocks/UAKelpBlock.java
+++ src/main/java/com/minecraftabnormals/upgrade_aquatic/common/blocks/UAKelpBlock.java
@@ -1,7 +1,9 @@
 package com.minecraftabnormals.upgrade_aquatic.common.blocks;
 
+import com.minecraftabnormals.upgrade_aquatic.core.registry.UABlocks;
 import com.minecraftabnormals.upgrade_aquatic.core.registry.UABlocks.KelpType;
 
+import net.minecraft.block.AbstractTopPlantBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.KelpBlock;
@@ -10,17 +12,32 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 
 public class UAKelpBlock extends KelpBlock {
-	@SuppressWarnings("unused")
 	private KelpType kelpType;
 	private Block kelpTopBlock;
-	
+
 	public UAKelpBlock(KelpType kelpType, Block kelpTopBlock, Properties props) {
 		super(props);
 		this.kelpType = kelpType;
 		this.kelpTopBlock = kelpTopBlock;
 	}
-	
+
+	@Override
 	public ItemStack getItem(IBlockReader worldIn, BlockPos pos, BlockState state) {
 		return new ItemStack(this.kelpTopBlock);
+	}
+
+	@Override
+	protected AbstractTopPlantBlock getTopPlantBlock() {
+		switch (this.kelpType) {
+		default:
+		case TONGUE:
+			return (AbstractTopPlantBlock) UABlocks.TONGUE_KELP.get();
+		case THORNY:
+			return (AbstractTopPlantBlock) UABlocks.THORNY_KELP.get();
+		case OCHRE:
+			return (AbstractTopPlantBlock) UABlocks.OCHRE_KELP.get();
+		case POLAR:
+			return (AbstractTopPlantBlock) UABlocks.POLAR_KELP.get();
+		}
 	}
 }
