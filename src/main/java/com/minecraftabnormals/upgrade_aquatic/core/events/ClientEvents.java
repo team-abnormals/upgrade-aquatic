9a842ac89cea1808d2c88ab93c8bcb7d6d815541
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-04-11 20:24:21 -0500

    Update ClientEvents.java

--- src/main/java/com/minecraftabnormals/upgrade_aquatic/core/events/ClientEvents.java
+++ src/main/java/com/minecraftabnormals/upgrade_aquatic/core/events/ClientEvents.java
@@ -6,6 +6,7 @@ import com.minecraftabnormals.upgrade_aquatic.core.UpgradeAquatic;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
@@ -22,12 +23,12 @@ import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 @Mod.EventBusSubscriber(modid = UpgradeAquatic.MOD_ID, value = Dist.CLIENT)
 public class ClientEvents {
-	
+
 	@SubscribeEvent
 	public static void onEntityRenderPre(RenderLivingEvent.Pre<?, ?> event) {
-		if(event.getEntity() instanceof ClientPlayerEntity) {
+		if (event.getEntity() instanceof ClientPlayerEntity) {
 			ClientPlayerEntity clientPlayer = (ClientPlayerEntity) event.getEntity();
-			if(clientPlayer.getRidingEntity() instanceof ThrasherEntity) {
+			if (clientPlayer.getRidingEntity() instanceof ThrasherEntity) {
 				ThrasherEntity thrasher = (ThrasherEntity) clientPlayer.getRidingEntity();
 				ObfuscationReflectionHelper.setPrivateValue(LivingEntity.class, clientPlayer, 1.0F, "field_205017_bL");
 				ObfuscationReflectionHelper.setPrivateValue(LivingEntity.class, clientPlayer, 1.0F, "field_205018_bM");
@@ -37,20 +38,20 @@ public class ClientEvents {
 			}
 		}
 	}
-	
+
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void onPlayerRenderPre(RenderPlayerEvent.Pre event) {
 		MatrixStack stack = event.getMatrixStack();
 		stack.push();
-		if(event.getEntityLiving().getRidingEntity() instanceof ThrasherEntity) {
+		if (event.getEntityLiving().getRidingEntity() instanceof ThrasherEntity) {
 			ThrasherEntity thrasher = (ThrasherEntity) event.getEntityLiving().getRidingEntity();
 			double dx = Math.cos((MathHelper.lerp(event.getPartialRenderTick(), thrasher.prevRotationYaw, thrasher.rotationYaw)) * Math.PI / 180.0D);
 			double dz = Math.sin((MathHelper.lerp(event.getPartialRenderTick(), thrasher.prevRotationYaw, thrasher.rotationYaw)) * Math.PI / 180.0D);
-		
+
 			stack.translate((float) dx, 0.0F, (float) dz);
 		}
 	}
-	
+
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void onPlayerRenderPost(RenderPlayerEvent.Post event) {
 		event.getMatrixStack().pop();
@@ -60,8 +61,11 @@ public class ClientEvents {
 	public static void onItemTooltip(ItemTooltipEvent event) {
 		Item item = event.getItemStack().getItem();
 		ResourceLocation name = item.getRegistryName();
+		PlayerEntity player = event.getPlayer();
+		if (name == null || player == null)
+			return;
 
-		if (name != null && UAConfig.CLIENT.showUnobtainableDescription.get() && name.getNamespace().equals(UpgradeAquatic.MOD_ID)) {
+		if (player.abilities.isCreativeMode && UAConfig.CLIENT.showUnobtainableDescription.get() && name.getNamespace().equals(UpgradeAquatic.MOD_ID)) {
 			String id = name.getPath();
 			if (id.contains("jelly") || id.contains("tongue_kelp") || id.contains("polar_kelp") || id.contains("ochre_kelp") || id.contains("thorny_kelp"))
 				event.getToolTip().add(new TranslationTextComponent("tooltip.upgrade_aquatic.unobtainable").mergeStyle(TextFormatting.GRAY));
