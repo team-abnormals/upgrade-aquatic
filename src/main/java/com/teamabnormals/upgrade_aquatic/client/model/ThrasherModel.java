5975dcfb66de926463d0515588987570fb1e60c8
SmellyModder <smellysocks543@gmail.com>
2022-07-21 18:54:03 -0400

    Fixed some models and animations

--- src/main/java/com/teamabnormals/upgrade_aquatic/client/model/ThrasherModel.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/client/model/ThrasherModel.java
@@ -1,5 +1,6 @@
 package com.teamabnormals.upgrade_aquatic.client.model;
 
+import com.teamabnormals.blueprint.core.endimator.Endimator;
 import com.teamabnormals.blueprint.core.endimator.entity.EndimatorEntityModel;
 import com.teamabnormals.upgrade_aquatic.common.entity.monster.Thrasher;
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -38,36 +39,37 @@ public class ThrasherModel<E extends Thrasher> extends EndimatorEntityModel<E> {
 
 	public ThrasherModel(ModelPart root) {
 		this.neck = root.getChild("neck");
-		this.body = root.getChild("body");
-		this.top_jaw = root.getChild("top_jaw");
-		this.bottom_jaw = root.getChild("bottom_jaw");
-		this.left_fin = root.getChild("left_fin");
-		this.right_fin = root.getChild("right_fin");
-		this.fin = root.getChild("fin");
-		this.tail_holder = root.getChild("tail_holder");
-		this.right_fin_2 = root.getChild("right_fin_2");
-		this.left_fin_2 = root.getChild("left_fin_2");
-		this.tail_holder_2 = root.getChild("tail_holder_2");
-		this.tail = root.getChild("tail");
-		this.tail_linear = root.getChild("tail_linear");
+		this.body = this.neck.getChild("body");
+		this.top_jaw = this.neck.getChild("top_jaw");
+		this.bottom_jaw = this.neck.getChild("bottom_jaw");
+		this.left_fin = this.neck.getChild("left_fin");
+		this.right_fin = this.neck.getChild("right_fin");
+		this.fin = this.neck.getChild("fin");
+		this.tail_holder = this.body.getChild("tail_holder");
+		this.right_fin_2 = this.body.getChild("right_fin_2");
+		this.left_fin_2 = this.body.getChild("left_fin_2");
+		this.tail_holder_2 = this.tail_holder.getChild("tail_holder_2");
+		this.tail = this.tail_holder_2.getChild("tail");
+		this.tail_linear = this.tail.getChild("tail_linear");
+		this.endimator = Endimator.compile(root);
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition root = meshdefinition.getRoot();
-		PartDefinition neck = root.addOrReplaceChild("neck", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 17.0F, -10.0F, 0.0F, 0.0F, 0.0F));
-		PartDefinition body = root.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, -4.0F, 4.0F, 0.0F, 0.0F, 0.0F));
-		PartDefinition top_jaw = root.addOrReplaceChild("top_jaw", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, -2.5F, -4.0F, 0.0F, 0.0F, 0.0F));
-		PartDefinition bottom_jaw = root.addOrReplaceChild("bottom_jaw", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 4.0F, -4.0F, 0.0F, 0.0F, 0.0F));
-		PartDefinition left_fin = root.addOrReplaceChild("left_fin", CubeListBuilder.create(), PartPose.offsetAndRotation(6.0F, 0.5F, -0.5F, 0.0F, -0.17453292F, 0.20943952F));
-		PartDefinition right_fin = root.addOrReplaceChild("right_fin", CubeListBuilder.create(), PartPose.offsetAndRotation(-6.0F, 0.5F, -0.5F, 0.0F, 0.17453292F, -0.20943952F));
-		PartDefinition fin = root.addOrReplaceChild("fin", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, -5.0F, -2.5F, -0.17453292F, 0.0F, 0.0F));
-		PartDefinition tail_holder = root.addOrReplaceChild("tail_holder", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 2.5F, 13.0F, 0.0F, 0.0F, 0.0F));
-		PartDefinition right_fin_2 = root.addOrReplaceChild("right_fin_2", CubeListBuilder.create(), PartPose.offsetAndRotation(-6.0F, 5.0F, 6.0F, 0.0F, 0.17453292F, -0.20943952F));
-		PartDefinition left_fin_2 = root.addOrReplaceChild("left_fin_2", CubeListBuilder.create(), PartPose.offsetAndRotation(6.0F, 5.0F, 6.0F, 0.0F, -0.17453292F, 0.20943952F));
-		PartDefinition tail_holder_2 = root.addOrReplaceChild("tail_holder_2", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 2.5F, 8.0F, 0.0F, 0.0F, 0.0F));
-		PartDefinition tail = root.addOrReplaceChild("tail", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 0.1F, 8.0F, 0.0F, 0.0F, 0.0F));
-		PartDefinition tail_linear = root.addOrReplaceChild("tail_linear", CubeListBuilder.create(), PartPose.offsetAndRotation(-3.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F));
+		PartDefinition neck = root.addOrReplaceChild("neck", CubeListBuilder.create().texOffs(0, 24).addBox(-6.0F, -5.0F, -4.0F, 12, 11, 8), PartPose.offsetAndRotation(0.0F, 17.0F, -10.0F, 0.0F, 0.0F, 0.0F));
+		PartDefinition body = neck.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 43).addBox(-6.0F, 0.0F, 0.0F, 12, 10, 13), PartPose.offsetAndRotation(0.0F, -4.0F, 4.0F, 0.0F, 0.0F, 0.0F));
+		PartDefinition top_jaw = neck.addOrReplaceChild("top_jaw", CubeListBuilder.create().texOffs(0, 0).addBox(-6.0F, -2.5F, -10.0F, 12, 5, 10), PartPose.offsetAndRotation(0.0F, -2.5F, -4.0F, 0.0F, 0.0F, 0.0F));
+		PartDefinition bottom_jaw = neck.addOrReplaceChild("bottom_jaw", CubeListBuilder.create().texOffs(44, 0).addBox(-6.0F, -2.0F, -10.0F, 12, 4, 10), PartPose.offsetAndRotation(0.0F, 4.0F, -4.0F, 0.0F, 0.0F, 0.0F));
+		PartDefinition left_fin = neck.addOrReplaceChild("left_fin", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(0.0F, 0.0F, -2.5F, 10, 1, 6), PartPose.offsetAndRotation(6.0F, 0.5F, -0.5F, 0.0F, -0.17453292F, 0.20943952F));
+		PartDefinition right_fin = neck.addOrReplaceChild("right_fin", CubeListBuilder.create().texOffs(0, 16).addBox(-10.0F, 0.0F, -2.5F, 10, 1, 6), PartPose.offsetAndRotation(-6.0F, 0.5F, -0.5F, 0.0F, 0.17453292F, -0.20943952F));
+		PartDefinition fin = neck.addOrReplaceChild("fin", CubeListBuilder.create().texOffs(50, 50).addBox(0.0F, -7.0F, 0.0F, 0, 7, 12), PartPose.offsetAndRotation(0.0F, -5.0F, -2.5F, -0.17453292F, 0.0F, 0.0F));
+		PartDefinition tail_holder = body.addOrReplaceChild("tail_holder", CubeListBuilder.create().texOffs(0, 66).addBox(-4.0F, -1.0F, 0.0F, 8, 7, 8), PartPose.offsetAndRotation(0.0F, 2.5F, 13.0F, 0.0F, 0.0F, 0.0F));
+		PartDefinition right_fin_2 = body.addOrReplaceChild("right_fin_2", CubeListBuilder.create().texOffs(33, 16).addBox(-13.0F, 0.0F, -4.0F, 13, 1, 8), PartPose.offsetAndRotation(-6.0F, 5.0F, 6.0F, 0.0F, 0.17453292F, -0.20943952F));
+		PartDefinition left_fin_2 = body.addOrReplaceChild("left_fin_2", CubeListBuilder.create().texOffs(33, 16).mirror().addBox(-0.0F, 0.0F, -4.0F, 13, 1, 8), PartPose.offsetAndRotation(6.0F, 5.0F, 6.0F, 0.0F, -0.17453292F, 0.20943952F));
+		PartDefinition tail_holder_2 = tail_holder.addOrReplaceChild("tail_holder_2", CubeListBuilder.create().texOffs(0, 81).addBox(-3.0F, -3.0F, 0.0F, 6, 6, 8), PartPose.offsetAndRotation(0.0F, 2.5F, 8.0F, 0.0F, 0.0F, 0.0F));
+		PartDefinition tail = tail_holder_2.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(30, 70).addBox(0.0F, -3.0F, 0.0F, 0, 6, 18), PartPose.offsetAndRotation(0.0F, 0.1F, 8.0F, 0.0F, 0.0F, 0.0F));
+		PartDefinition tail_linear = tail.addOrReplaceChild("tail_linear", CubeListBuilder.create().texOffs(30, 70).addBox(0.0F, 0.0F, 0.0F, 6, 0, 18), PartPose.offsetAndRotation(-3.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F));
 		return LayerDefinition.create(meshdefinition, 100, 100);
 	}
 
