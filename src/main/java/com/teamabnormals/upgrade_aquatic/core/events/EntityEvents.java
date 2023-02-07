7140515afe5cd7c8fa9941452e92b365fe7c0f0d
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-02-07 13:15:47 -0600

    Reimplement Coral reviving via Glowing Ink

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/events/EntityEvents.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/events/EntityEvents.java
@@ -1,5 +1,6 @@
 package com.teamabnormals.upgrade_aquatic.core.events;
 
+import com.teamabnormals.blueprint.core.util.BlockUtil;
 import com.teamabnormals.blueprint.core.util.TradeUtil;
 import com.teamabnormals.blueprint.core.util.TradeUtil.BlueprintTrade;
 import com.teamabnormals.upgrade_aquatic.api.util.UAEntityPredicates;
@@ -16,13 +17,18 @@ import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction.Axis;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.protocol.game.ClientboundAwardStatsPacket;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stat;
 import net.minecraft.stats.Stats;
 import net.minecraft.stats.StatsCounter;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
@@ -41,6 +47,8 @@ import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
@@ -49,6 +57,7 @@ import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.EntityMountEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingTickEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.event.entity.player.PlayerSetSpawnEvent;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.event.village.VillagerTradesEvent;
@@ -56,6 +65,7 @@ import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = UpgradeAquatic.MOD_ID)
 public class EntityEvents {
@@ -97,8 +107,7 @@ public class EntityEvents {
 		}
 		if (entity instanceof Drowned && UAConfig.COMMON.drownedSwimmingAnimation.get() && entity.isEffectiveAi()) {
 			Pose pose = entity.getDeltaMovement().horizontalDistanceSqr() >= 0.000625F && entity.getCommandSenderWorld().getFluidState(entity.blockPosition().below()).is(FluidTags.WATER) ? Pose.SWIMMING : Pose.STANDING;
-			if (entity.getPose() != pose)
-				entity.setPose(pose);
+			if (entity.getPose() != pose) entity.setPose(pose);
 		}
 	}
 
@@ -119,8 +128,7 @@ public class EntityEvents {
 	public static void onPlayerSetSpawn(PlayerSetSpawnEvent event) {
 		Entity entity = event.getEntity();
 		BlockPos spawn = event.getNewSpawn();
-		if (spawn != null && entity.getCommandSenderWorld().getBlockState(spawn).getBlock() instanceof BedrollBlock)
-			event.setCanceled(true);
+		if (spawn != null && entity.getCommandSenderWorld().getBlockState(spawn).getBlock() instanceof BedrollBlock) event.setCanceled(true);
 	}
 
 	@SubscribeEvent
@@ -194,89 +202,64 @@ public class EntityEvents {
 	}
 
 	@SubscribeEvent
-	public static void onWandererTradesEvent(WandererTradesEvent event) {
-		TradeUtil.addWandererTrades(event,
-				new BlueprintTrade(1, UABlocks.BEACHGRASS.get().asItem(), 1, 12, 1),
-				new BlueprintTrade(1, UABlocks.WHITE_SEAROCKET.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(1, UABlocks.PINK_SEAROCKET.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(1, UABlocks.BLUE_PICKERELWEED.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(1, UABlocks.PURPLE_PICKERELWEED.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.FINGER_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.ACAN_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.BRANCH_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.PILLOW_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.SILK_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.PETAL_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.MOSS_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.ROCK_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.STAR_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(3, UABlocks.CHROME_CORAL_BLOCK.get().asItem(), 1, 8, 1),
-				new BlueprintTrade(5, UABlocks.EMBEDDED_AMMONITE.get().asItem(), 1, 5, 1)
-		);
+	public static void onRightClickBlock(RightClickBlock event) {
+		Level level = event.getLevel();
+		BlockPos pos = event.getPos();
+		BlockState state = level.getBlockState(pos);
+		Player player = event.getEntity();
+		ItemStack stack = event.getItemStack();
+		RandomSource random = player.getRandom();
 
-		TradeUtil.addRareWandererTrades(event,
-				new BlueprintTrade(2, UABlocks.DRIFTWOOD_LOG.get().asItem(), 1, 16, 1),
-				new BlueprintTrade(5, UAItems.PIKE_BUCKET.get(), 1, 4, 1),
-				new BlueprintTrade(5, UAItems.LIONFISH_BUCKET.get(), 1, 4, 1),
-				new BlueprintTrade(5, UAItems.NAUTILUS_BUCKET.get(), 1, 4, 1)
-		);
+		if (stack.is(Items.GLOW_INK_SAC)) {
+			ResourceLocation name = ForgeRegistries.BLOCKS.getKey(state.getBlock());
+			String path = name.getPath();
+			if ((path.contains("dead") || path.contains("elder")) && path.contains("coral") && !path.contains("coralstone")) {
+				Block livingCoral = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(name.getNamespace(), path.replace("dead_", "").replace("elder_", "")));
+				if (livingCoral != null) {
+					level.setBlockAndUpdate(pos, BlockUtil.transferAllBlockStates(state, livingCoral.defaultBlockState()));
+					level.scheduleTick(pos, livingCoral, 60 + level.getRandom().nextInt(40));
+					level.playSound(player, pos, SoundEvents.SQUID_SQUIRT, SoundSource.BLOCKS, 1.0F, 1.0F);
+					for (int i = 0; i < 5; ++i) {
+						double d1 = random.nextGaussian() * 0.02D;
+						double d6 = (double) pos.getX() + random.nextDouble();
+						double d7 = (double) pos.getY() + random.nextDouble() * state.getShape(level, pos).max(Axis.Y);
+						double d8 = (double) pos.getZ() + random.nextDouble();
+						level.addParticle(ParticleTypes.GLOW_SQUID_INK, d6, d7, d8, d1, d1, d1);
+					}
+					if (!player.getAbilities().instabuild) player.getItemInHand(event.getHand()).shrink(1);
+					event.setCancellationResult(InteractionResult.sidedSuccess(level.isClientSide()));
+					event.setCanceled(true);
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void onWandererTradesEvent(WandererTradesEvent event) {
+		TradeUtil.addWandererTrades(event, new BlueprintTrade(1, UABlocks.BEACHGRASS.get().asItem(), 1, 12, 1), new BlueprintTrade(1, UABlocks.WHITE_SEAROCKET.get().asItem(), 1, 8, 1), new BlueprintTrade(1, UABlocks.PINK_SEAROCKET.get().asItem(), 1, 8, 1), new BlueprintTrade(1, UABlocks.BLUE_PICKERELWEED.get().asItem(), 1, 8, 1), new BlueprintTrade(1, UABlocks.PURPLE_PICKERELWEED.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.FINGER_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.ACAN_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.BRANCH_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.PILLOW_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.SILK_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.PETAL_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.MOSS_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.ROCK_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.STAR_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(3, UABlocks.CHROME_CORAL_BLOCK.get().asItem(), 1, 8, 1), new BlueprintTrade(5, UABlocks.EMBEDDED_AMMONITE.get().asItem(), 1, 5, 1));
+
+		TradeUtil.addRareWandererTrades(event, new BlueprintTrade(2, UABlocks.DRIFTWOOD_LOG.get().asItem(), 1, 16, 1), new BlueprintTrade(5, UAItems.PIKE_BUCKET.get(), 1, 4, 1), new BlueprintTrade(5, UAItems.LIONFISH_BUCKET.get(), 1, 4, 1), new BlueprintTrade(5, UAItems.NAUTILUS_BUCKET.get(), 1, 4, 1));
 	}
 
 	@SubscribeEvent
 	public static void onVillagerTradesEvent(VillagerTradesEvent event) {
-		TradeUtil.addVillagerTrades(event, VillagerProfession.FARMER, TradeUtil.APPRENTICE,
-				new BlueprintTrade(1, UAItems.MULBERRY.get(), 5, 16, 5)
-		);
+		TradeUtil.addVillagerTrades(event, VillagerProfession.FARMER, TradeUtil.APPRENTICE, new BlueprintTrade(1, UAItems.MULBERRY.get(), 5, 16, 5));
 
 		if (event.getType().equals(VillagerProfession.FISHERMAN)) {
-			TradeUtil.addVillagerTrades(event, TradeUtil.APPRENTICE,
-					new BlueprintTrade(new ItemStack(UAItems.PERCH.get(), 6), new ItemStack(Items.EMERALD), new ItemStack(UAItems.COOKED_PERCH.get(), 6), 16, 5, 0.05F)
-			);
+			TradeUtil.addVillagerTrades(event, TradeUtil.APPRENTICE, new BlueprintTrade(new ItemStack(UAItems.PERCH.get(), 6), new ItemStack(Items.EMERALD), new ItemStack(UAItems.COOKED_PERCH.get(), 6), 16, 5, 0.05F));
 
-			TradeUtil.addVillagerTrades(event, TradeUtil.JOURNEYMAN,
-					new BlueprintTrade(UAItems.PERCH.get(), 14, 1, 16, 20),
-					new BlueprintTrade(new ItemStack(UAItems.PIKE.get(), 6), new ItemStack(Items.EMERALD), new ItemStack(UAItems.COOKED_PIKE.get(), 6), 16, 10, 0.05F)
-			);
+			TradeUtil.addVillagerTrades(event, TradeUtil.JOURNEYMAN, new BlueprintTrade(UAItems.PERCH.get(), 14, 1, 16, 20), new BlueprintTrade(new ItemStack(UAItems.PIKE.get(), 6), new ItemStack(Items.EMERALD), new ItemStack(UAItems.COOKED_PIKE.get(), 6), 16, 10, 0.05F));
 
-			TradeUtil.addVillagerTrades(event, TradeUtil.EXPERT,
-					new BlueprintTrade(UAItems.PIKE.get(), 5, 1, 16, 25),
-					new BlueprintTrade(new ItemStack(UAItems.LIONFISH.get(), 6), new ItemStack(Items.EMERALD), new ItemStack(UAItems.COOKED_LIONFISH.get(), 6), 16, 15, 0.05F)
-			);
+			TradeUtil.addVillagerTrades(event, TradeUtil.EXPERT, new BlueprintTrade(UAItems.PIKE.get(), 5, 1, 16, 25), new BlueprintTrade(new ItemStack(UAItems.LIONFISH.get(), 6), new ItemStack(Items.EMERALD), new ItemStack(UAItems.COOKED_LIONFISH.get(), 6), 16, 15, 0.05F));
 
-			TradeUtil.addVillagerTrades(event, TradeUtil.MASTER,
-					new BlueprintTrade(UAItems.LIONFISH.get(), 3, 1, 16, 30)
-			);
+			TradeUtil.addVillagerTrades(event, TradeUtil.MASTER, new BlueprintTrade(UAItems.LIONFISH.get(), 3, 1, 16, 30));
 		}
 
-		TradeUtil.addVillagerTrades(event, VillagerProfession.MASON, TradeUtil.MASTER,
-				new BlueprintTrade(5, UABlocks.TOOTH_TILES.get().asItem(), 1, 12, 30),
-				new BlueprintTrade(5, UABlocks.TOOTH_BRICKS.get().asItem(), 1, 12, 30)
-		);
+		TradeUtil.addVillagerTrades(event, VillagerProfession.MASON, TradeUtil.MASTER, new BlueprintTrade(5, UABlocks.TOOTH_TILES.get().asItem(), 1, 12, 30), new BlueprintTrade(5, UABlocks.TOOTH_BRICKS.get().asItem(), 1, 12, 30));
 
-		if (UAConfig.COMMON.clericsBuyThrasherTeeth.get())
-			TradeUtil.addVillagerTrades(event, VillagerProfession.CLERIC, TradeUtil.EXPERT,
-					new BlueprintTrade(UAItems.THRASHER_TOOTH.get(), 1, 1, 12, 15)
-			);
+		if (UAConfig.COMMON.clericsBuyThrasherTeeth.get()) TradeUtil.addVillagerTrades(event, VillagerProfession.CLERIC, TradeUtil.EXPERT, new BlueprintTrade(UAItems.THRASHER_TOOTH.get(), 1, 1, 12, 15));
 
 		if (UAConfig.COMMON.leatherworkersSellBedrolls.get())
-			TradeUtil.addVillagerTrades(event, VillagerProfession.LEATHERWORKER, TradeUtil.APPRENTICE,
-					new BlueprintTrade(1, UABlocks.BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.WHITE_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.ORANGE_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.MAGENTA_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.LIGHT_BLUE_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.YELLOW_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.LIME_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.PINK_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.GRAY_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.LIGHT_GRAY_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.CYAN_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.PURPLE_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.BLUE_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.BROWN_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.GREEN_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.RED_BEDROLL.get().asItem(), 1, 8, 10),
-					new BlueprintTrade(1, UABlocks.BLACK_BEDROLL.get().asItem(), 1, 8, 10)
-			);
+			TradeUtil.addVillagerTrades(event, VillagerProfession.LEATHERWORKER, TradeUtil.APPRENTICE, new BlueprintTrade(1, UABlocks.BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.WHITE_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.ORANGE_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.MAGENTA_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.LIGHT_BLUE_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.YELLOW_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.LIME_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.PINK_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.GRAY_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.LIGHT_GRAY_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.CYAN_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.PURPLE_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.BLUE_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.BROWN_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.GREEN_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.RED_BEDROLL.get().asItem(), 1, 8, 10), new BlueprintTrade(1, UABlocks.BLACK_BEDROLL.get().asItem(), 1, 8, 10));
 	}
 }
