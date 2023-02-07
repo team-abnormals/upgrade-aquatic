95a7acad0e5fd244267c645d5588781e96953d66
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-02-06 22:27:24 -0600

    Fix Turtle Shell icons not rendering

--- src/main/java/com/teamabnormals/upgrade_aquatic/client/renderer/RenderOverlays.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/client/renderer/RenderOverlays.java
@@ -3,17 +3,26 @@ package com.teamabnormals.upgrade_aquatic.client.renderer;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
+import com.teamabnormals.upgrade_aquatic.common.entity.monster.Thrasher;
 import com.teamabnormals.upgrade_aquatic.core.UAConfig;
 import com.teamabnormals.upgrade_aquatic.core.UpgradeAquatic;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.stats.Stats;
 import net.minecraft.stats.StatsCounter;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Mth;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -68,73 +77,72 @@ public class RenderOverlays {
 		}
 	}
 
-//	@SubscribeEvent
-//	public static void renderScuteOverAir(RenderGuiOverlayEvent.Pre event) {
-//		if (event.getOverlay() == VanillaGuiOverlay.AIR_LEVEL.type()) {
-//			int scaledWidth = MC.getWindow().getGuiScaledWidth();
-//			int scaledHeight = MC.getWindow().getGuiScaledHeight();
-//			LocalPlayer player = MC.player;
-//			boolean inWater = player.isEyeInFluid(FluidTags.WATER);
-//			if (inWater) {
-//				ItemStack turtleHelmet = ItemStack.EMPTY;
-//				for (ItemStack stack : player.getArmorSlots()) {
-//					if (stack.getItem() == Items.TURTLE_HELMET) {
-//						turtleHelmet = stack;
-//					}
-//				}
-//
-//				if (!turtleHelmet.isEmpty()) {
-//					ForgeIngameGui forgeIngameGui = (ForgeIngameGui) MC.gui;
-//					event.setCanceled(true);
-//
-//					PoseStack stack = event.getPoseStack();
-//					stack.pushPose();
-//					RenderSystem.enableBlend();
-//					int left = scaledWidth / 2 + 91;
-//					int top = scaledHeight - forgeIngameGui.right_height;
-//					int durability = turtleHelmet.getDamageValue();
-//					int maxDurability = turtleHelmet.getMaxDamage();
-//
-//					RenderSystem.setShaderTexture(0, new ResourceLocation(UpgradeAquatic.MOD_ID, "textures/gui/overlay/scute_bubble_depleted.png"));
-//					for (int i = 0; i < 10; i++) {
-//						int l = left - (i * 8) - 9;
-//						int l2 = l + 9;
-//						int t = top;
-//						int t2 = t + 9;
-//						Tesselator tessellator = Tesselator.getInstance();
-//						BufferBuilder bufferbuilder = tessellator.getBuilder();
-//						bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-//						bufferbuilder.vertex(l, t2, 0).uv(0, 1).endVertex();
-//						bufferbuilder.vertex(l2, t2, 0).uv(1, 1).endVertex();
-//						bufferbuilder.vertex(l2, t, 0).uv(1, 0).endVertex();
-//						bufferbuilder.vertex(l, t, 0).uv(0, 0).endVertex();
-//						tessellator.end();
-//					}
-//					RenderSystem.setShaderTexture(0, new ResourceLocation(UpgradeAquatic.MOD_ID, "textures/gui/overlay/scute_bubble.png"));
-//					double amount = Mth.clamp(10 - Math.floor((double) durability / maxDurability * 10.0), 1, 10);
-//					for (int i = 0; i < amount; i++) {
-//						int l = left - (i * 8) - 9;
-//						int l2 = l + 9;
-//						int t = top;
-//						int t2 = t + 9;
-//						Tesselator tessellator = Tesselator.getInstance();
-//						BufferBuilder bufferbuilder = tessellator.getBuilder();
-//						bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-//						bufferbuilder.vertex(l, t2, 0).uv(0, 1).endVertex();
-//						bufferbuilder.vertex(l2, t2, 0).uv(1, 1).endVertex();
-//						bufferbuilder.vertex(l2, t, 0).uv(1, 0).endVertex();
-//						bufferbuilder.vertex(l, t, 0).uv(0, 0).endVertex();
-//						tessellator.end();
-//					}
-//					forgeIngameGui.right_height += 10;
-//
-//					RenderSystem.disableBlend();
-//					stack.popPose();
-//				}
-//			}
-//		}
-//		if (event.getType() == ElementType.TEXT && MC.player.isPassenger() && MC.player.getVehicle() instanceof Thrasher && MC.gui.overlayMessageString.getString().equals(I18n.get("mount.onboard", I18n.get("key.keyboard.left.shift")))) {
-//			MC.gui.setOverlayMessage(Component.literal(""), false);
-//		}
-//	}
+	@SubscribeEvent
+	public static void renderScuteOverAir(RenderGuiOverlayEvent.Pre event) {
+		if (event.getOverlay() == VanillaGuiOverlay.AIR_LEVEL.type()) {
+			int scaledWidth = MC.getWindow().getGuiScaledWidth();
+			int scaledHeight = MC.getWindow().getGuiScaledHeight();
+			ForgeGui forgeGui = (ForgeGui) MC.gui;
+			LocalPlayer player = MC.player;
+			boolean inWater = player.isEyeInFluid(FluidTags.WATER);
+			if (inWater && forgeGui.shouldDrawSurvivalElements()) {
+				ItemStack turtleHelmet = ItemStack.EMPTY;
+				for (ItemStack stack : player.getArmorSlots()) {
+					if (stack.getItem() == Items.TURTLE_HELMET) {
+						turtleHelmet = stack;
+					}
+				}
+
+				if (!turtleHelmet.isEmpty()) {
+					event.setCanceled(true);
+
+					PoseStack stack = event.getPoseStack();
+					stack.pushPose();
+					RenderSystem.enableBlend();
+					int left = scaledWidth / 2 + 91;
+					int top = scaledHeight - forgeGui.rightHeight;
+					int durability = turtleHelmet.getDamageValue();
+					int maxDurability = turtleHelmet.getMaxDamage();
+
+					RenderSystem.setShaderTexture(0, new ResourceLocation(UpgradeAquatic.MOD_ID, "textures/gui/overlay/scute_bubble_depleted.png"));
+					for (int i = 0; i < 10; i++) {
+						int l = left - (i * 8) - 9;
+						int l2 = l + 9;
+						int t = top;
+						int t2 = t + 9;
+						Tesselator tessellator = Tesselator.getInstance();
+						BufferBuilder bufferbuilder = tessellator.getBuilder();
+						bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+						bufferbuilder.vertex(l, t2, 0).uv(0, 1).endVertex();
+						bufferbuilder.vertex(l2, t2, 0).uv(1, 1).endVertex();
+						bufferbuilder.vertex(l2, t, 0).uv(1, 0).endVertex();
+						bufferbuilder.vertex(l, t, 0).uv(0, 0).endVertex();
+						tessellator.end();
+					}
+					RenderSystem.setShaderTexture(0, new ResourceLocation(UpgradeAquatic.MOD_ID, "textures/gui/overlay/scute_bubble.png"));
+					double amount = Mth.clamp(10 - Math.floor((double) durability / maxDurability * 10.0), 1, 10);
+					for (int i = 0; i < amount; i++) {
+						int l = left - (i * 8) - 9;
+						int l2 = l + 9;
+						int t = top;
+						int t2 = t + 9;
+						Tesselator tessellator = Tesselator.getInstance();
+						BufferBuilder bufferbuilder = tessellator.getBuilder();
+						bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+						bufferbuilder.vertex(l, t2, 0).uv(0, 1).endVertex();
+						bufferbuilder.vertex(l2, t2, 0).uv(1, 1).endVertex();
+						bufferbuilder.vertex(l2, t, 0).uv(1, 0).endVertex();
+						bufferbuilder.vertex(l, t, 0).uv(0, 0).endVertex();
+						tessellator.end();
+					}
+					forgeGui.rightHeight += 10;
+
+					RenderSystem.disableBlend();
+					stack.popPose();
+				}
+			}
+		}
+
+	}
+
 }
