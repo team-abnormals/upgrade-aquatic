37ffa01540eb85c7c067378c4dfcac6979b87bca
Alexis GEORGES <georges.alexis@outlook.com>
2023-05-16 14:12:24 +0200

    Update UABiomeTags.java

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/other/tags/UABiomeTags.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/other/tags/UABiomeTags.java
@@ -21,12 +21,12 @@ public class UABiomeTags {
 	public static final TagKey<Biome> HAS_GREAT_THRASHER = biomeTag("has_monster/great_thrasher");
 
 	public static final TagKey<Biome> HAS_AMMONITE_ORE = biomeTag("has_feature/ammonite_ore");
+	public static final TagKey<Biome> HAS_PRISMARINE_CORAL = biomeTag("has_feature/prismarine_coral");
 	public static final TagKey<Biome> HAS_BEACH_VEGETATION = biomeTag("has_feature/beach_vegetation");
 	public static final TagKey<Biome> HAS_RIVER_TREE = biomeTag("has_feature/river_tree");
 	public static final TagKey<Biome> HAS_FLOWERING_RUSH = biomeTag("has_feature/flowering_rush");
 	public static final TagKey<Biome> HAS_PICKERELWEED = biomeTag("has_feature/pickerelweed");
 	public static final TagKey<Biome> HAS_EXTRA_PICKERELWEED = biomeTag("has_feature/pickerelweed_extra");
-	public static final TagKey<Biome> PRISMARINE_CORAL = biomeTag("has_feature/prismarine_coral");
 	
 	public static final TagKey<Biome> HAS_DRIFTWOOD = biomeTag("has_feature/driftwood");
 	public static final TagKey<Biome> HAS_DRIFTWOOD_BEACH = biomeTag("has_feature/driftwood_beach");
