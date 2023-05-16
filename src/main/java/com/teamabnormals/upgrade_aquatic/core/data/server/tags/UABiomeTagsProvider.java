5c3cc36bc178e67413334de9e9bac4b240b01f50
Alexis GEORGES <georges.alexis@outlook.com>
2023-05-16 13:59:51 +0200

    Update UABiomeTagsProvider.java

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/data/server/tags/UABiomeTagsProvider.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/data/server/tags/UABiomeTagsProvider.java
@@ -19,21 +19,30 @@ public class UABiomeTagsProvider extends BiomeTagsProvider {
 	@Override
 	public void addTags() {
 		this.tag(UABiomeTags.HAS_NAUTILUS).add(Biomes.OCEAN, Biomes.DEEP_OCEAN, Biomes.WARM_OCEAN, Biomes.LUKEWARM_OCEAN, Biomes.DEEP_LUKEWARM_OCEAN);
-		this.tag(UABiomeTags.HAS_THRASHER).add(Biomes.COLD_OCEAN, Biomes.DEEP_COLD_OCEAN, Biomes.FROZEN_OCEAN).addTag(UABiomeTags.HAS_GREAT_THRASHER);
-		this.tag(UABiomeTags.HAS_GREAT_THRASHER).add(Biomes.DEEP_FROZEN_OCEAN);
+		this.tag(UABiomeTags.HAS_THRASHER).add(Biomes.COLD_OCEAN, Biomes.DEEP_COLD_OCEAN).addTag(UABiomeTags.HAS_GREAT_THRASHER);
+		this.tag(UABiomeTags.HAS_GREAT_THRASHER).add(Biomes.FROZEN_OCEAN, Biomes.DEEP_FROZEN_OCEAN);
 		this.tag(UABiomeTags.HAS_LIONFISH).add(Biomes.WARM_OCEAN);
 
-		this.tag(UABiomeTags.HAS_PIKE).add(Biomes.RIVER);
+		this.tag(UABiomeTags.HAS_PIKE).add(BiomeTags.IS_RIVER);
 		this.tag(UABiomeTags.HAS_EXTRA_PIKE).addTag(Tags.Biomes.IS_SWAMP);
-		this.tag(UABiomeTags.HAS_SQUID).add(Biomes.SWAMP);
-		this.tag(UABiomeTags.HAS_PERCH).add(Biomes.SWAMP);
+		this.tag(UABiomeTags.HAS_SQUID).addTag(Tags.Biomes.IS_SWAMP);
+		this.tag(UABiomeTags.HAS_PERCH).addTag(Tags.Biomes.IS_SWAMP);
 
 		this.tag(UABiomeTags.HAS_CASSIOPEA_JELLYFISH).add(Biomes.LUKEWARM_OCEAN, Biomes.DEEP_LUKEWARM_OCEAN);
 		this.tag(UABiomeTags.HAS_JELLYFISH).add(Biomes.WARM_OCEAN).addTag(UABiomeTags.HAS_CASSIOPEA_JELLYFISH);
 
-		this.tag(UABiomeTags.HAS_AMMONITE_ORE).addTag(BiomeTags.IS_BEACH).addTag(BiomeTags.IS_OCEAN).add(Biomes.STONY_SHORE);
-		this.tag(UABiomeTags.HAS_PICKERELWEED).addTag(BiomeTags.IS_JUNGLE).add(Biomes.RIVER).addTag(Tags.Biomes.IS_SWAMP);
-		this.tag(UABiomeTags.HAS_EXTRA_DRIFTWOOD).addOptional(new ResourceLocation("atmospheric", "rainforest_basin")).addOptional(new ResourceLocation("atmospheric", "sparse_rainforest_basin"));
+		this.tag(UABiomeTags.HAS_AMMONITE_ORE).addTag(BiomeTags.IS_BEACH).addTag(BiomeTags.IS_OCEAN);
+		this.tag(UABiomeTags.HAS_BEACH_VEGETATION).add(Biomes.BEACH);
+		this.tag(UABiomeTags.HAS_RIVER_TREE).add(Biomes.RIVER);
+		this.tag(UABiomeTags.HAS_FLOWERING_RUSH).add(Biomes.FLOWER_FOREST);
+		this.tag(UABiomeTags.HAS_PICKERELWEED).add(Biomes.RIVER).addTag(BiomeTags.IS_JUNGLE).addTag(Tags.Biomes.IS_SWAMP);
 		this.tag(UABiomeTags.HAS_EXTRA_PICKERELWEED).add(Biomes.FLOWER_FOREST);
+		this.tag(UABiomeTags.PRISMARINE_CORAL).addTag(BiomeTags.IS_OCEAN);
+		
+		this.tag(UABiomeTags.HAS_DRIFTWOOD).addTag(BiomeTags.IS_OCEAN);
+		this.tag(UABiomeTags.HAS_DRIFTWOOD_BEACH).addTag(BiomeTags.IS_BEACH);
+		this.tag(UABiomeTags.HAS_DRIFTWOOD_RIVER).addTag(BiomeTags.IS_RIVER);
+		this.tag(UABiomeTags.HAS_DRIFTWOOD_SWAMP).addTag(Tags.Biomes.IS_SWAMP);
+		this.tag(UABiomeTags.HAS_EXTRA_DRIFTWOOD).addOptional(new ResourceLocation("atmospheric", "rainforest_basin")).addOptional(new ResourceLocation("atmospheric", "sparse_rainforest_basin"));
 	}
 }
