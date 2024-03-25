6eb690e468de30a373a7d3ebc3880eebf9538189
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-03-25 01:16:29 -0500

    Fix an ordering

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/other/UAConstants.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/other/UAConstants.java
@@ -7,7 +7,7 @@ public class UAConstants {
 
 	public static final ResourceLocation BAMBOO_LADDER = new ResourceLocation("woodworks", "bamboo_ladder");
 	public static final ResourceLocation BAMBOO_BEEHIVE = new ResourceLocation("woodworks", "bamboo_beehive");
-	public static final ResourceLocation CHISELED_BAMBOO_BOOKSHELF = new ResourceLocation("woodworks", "chiseled_bamboo_bookshelf");
+	public static final ResourceLocation BAMBOO_BOOKSHELF = new ResourceLocation("woodworks", "bamboo_bookshelf");
 	public static final ResourceLocation BAMBOO_CLOSET = new ResourceLocation("woodworks", "bamboo_closet");
 	public static final ResourceLocation TRAPPED_BAMBOO_CLOSET = new ResourceLocation("woodworks", "trapped_bamboo_closet");
 }
