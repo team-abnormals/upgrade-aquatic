069989b350ff16982576229745901d3ef59547e7
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-04-22 08:42:23 -0500

    Datagen many tags

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/data/server/tags/UABlockTagsProvider.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/data/server/tags/UABlockTagsProvider.java
@@ -1,56 +1,65 @@
 package com.teamabnormals.upgrade_aquatic.core.data.server.tags;
 
 import com.teamabnormals.blueprint.core.other.tags.BlueprintBlockTags;
+import com.teamabnormals.upgrade_aquatic.common.block.CoralType;
 import com.teamabnormals.upgrade_aquatic.core.UpgradeAquatic;
 import com.teamabnormals.upgrade_aquatic.core.other.tags.UABlockTags;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.*;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 import static com.teamabnormals.upgrade_aquatic.core.registry.UABlocks.*;
 
-//TODO: Add all the other block tags to this
 public class UABlockTagsProvider extends BlockTagsProvider {
+	private Collection<DeferredHolder<Block, ? extends Block>> entries;
 
 	public UABlockTagsProvider(PackOutput output, CompletableFuture<Provider> provider, ExistingFileHelper helper) {
 		super(output, provider, UpgradeAquatic.MOD_ID, helper);
+		this.entries = BLOCKS.getDeferredRegister().getEntries();
 	}
 
 	@Override
 	public void addTags(Provider provider) {
-		IntrinsicTagAppender<Block> mineableWithPickaxe = this.tag(BlockTags.MINEABLE_WITH_PICKAXE);
-		IntrinsicTagAppender<Block> mineableWithHoe = this.tag(BlockTags.MINEABLE_WITH_HOE);
-
 		this.tag(BlockTags.MINEABLE_WITH_AXE).add(MULBERRY_PUNNET.get());
-		mineableWithPickaxe.add(EMBEDDED_AMMONITE.get(), PRISMARINE_ROD_BUNDLE.get(), ELDER_EYE.get());
-		mineableWithHoe.add(RIVER_LEAVES.get());
+		this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(EMBEDDED_AMMONITE.get(), PRISMARINE_ROD_BUNDLE.get(), ELDER_EYE.get());
+		this.tag(BlockTags.MINEABLE_WITH_HOE).add(RIVER_LEAVES.get());
 
-		for (DeferredHolder<Block, ? extends Block> block : BLOCKS.getDeferredRegister().getEntries()) {
-			String path = block.getId().getPath();
+		for (DeferredHolder<Block, ? extends Block> holder : BLOCKS.getDeferredRegister().getEntries()) {
+			Block block = holder.get();
+			String path = holder.getId().getPath();
 			if (path.contains("luminous_prismarine") || (path.contains("dead") && path.contains("coral")) || path.contains("coral_block") || path.contains("tooth") || path.contains("scute") || path.contains("coralstone") || path.contains("kelpy_cobblestone") || path.contains("kelpy_stone")) {
-				mineableWithPickaxe.add(block.get());
+				this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block);
 			} else if (path.contains("beachgrass") || path.contains("pickerelweed_block") || path.contains("kelp_block")) {
-				mineableWithHoe.add(block.get());
+				this.tag(BlockTags.MINEABLE_WITH_HOE).add(block);
 			}
 		}
 
+		this.tag(BlockTags.STAIRS, is(StairBlock.class), BlockTags.WOODEN_STAIRS, DRIFTWOOD_STAIRS.get(), RIVER_STAIRS.get());
+		this.tag(BlockTags.SLABS, is(SlabBlock.class), BlockTags.WOODEN_SLABS, DRIFTWOOD_SLAB.get(), RIVER_SLAB.get());
+		this.tag(BlockTags.WALLS, is(WallBlock.class));
+		this.tag(BlockTags.DOORS, is(DoorBlock.class), BlockTags.WOODEN_DOORS, DRIFTWOOD_DOOR.get(), RIVER_DOOR.get());
+		this.tag(BlockTags.TRAPDOORS, is(TrapDoorBlock.class), BlockTags.WOODEN_TRAPDOORS, DRIFTWOOD_TRAPDOOR.get(), RIVER_TRAPDOOR.get());
+
+		this.tag(BlockTags.SMALL_FLOWERS, is(FlowerBlock.class));
+		this.tag(BlockTags.TALL_FLOWERS, is(TallFlowerBlock.class));
+		this.tag(BlockTags.FLOWER_POTS, is(FlowerPotBlock.class));
+
 		this.tag(BlockTags.PLANKS).add(DRIFTWOOD_PLANKS.get(), RIVER_PLANKS.get());
 		this.tag(BlockTags.LOGS_THAT_BURN).addTag(UABlockTags.DRIFTWOOD_LOGS).addTag(UABlockTags.RIVER_LOGS);
-		this.tag(BlockTags.WOODEN_SLABS).add(DRIFTWOOD_SLAB.get(), RIVER_SLAB.get());
-		this.tag(BlockTags.WOODEN_STAIRS).add(DRIFTWOOD_STAIRS.get(), RIVER_STAIRS.get());
 		this.tag(BlockTags.WOODEN_FENCES).add(DRIFTWOOD_FENCE.get(), RIVER_FENCE.get());
 		this.tag(BlockTags.FENCE_GATES).add(DRIFTWOOD_FENCE_GATE.get(), RIVER_FENCE_GATE.get());
 		this.tag(Tags.Blocks.FENCE_GATES_WOODEN).add(DRIFTWOOD_FENCE_GATE.get(), RIVER_FENCE_GATE.get());
-		this.tag(BlockTags.WOODEN_DOORS).add(DRIFTWOOD_DOOR.get(), RIVER_DOOR.get());
-		this.tag(BlockTags.WOODEN_TRAPDOORS).add(DRIFTWOOD_TRAPDOOR.get(), RIVER_TRAPDOOR.get());
 		this.tag(BlockTags.WOODEN_BUTTONS).add(DRIFTWOOD_BUTTON.get(), RIVER_BUTTON.get());
 		this.tag(BlockTags.WOODEN_PRESSURE_PLATES).add(DRIFTWOOD_PRESSURE_PLATE.get(), RIVER_PRESSURE_PLATE.get());
 		this.tag(BlockTags.STANDING_SIGNS).add(DRIFTWOOD_SIGNS.getFirst().get(), RIVER_SIGNS.getFirst().get());
@@ -60,6 +69,11 @@ public class UABlockTagsProvider extends BlockTagsProvider {
 		this.tag(BlockTags.OVERWORLD_NATURAL_LOGS).add(DRIFTWOOD_LOG.get(), RIVER_LOG.get());
 		this.tag(BlockTags.LEAVES).add(RIVER_LEAVES.get());
 		this.tag(BlockTags.SAPLINGS).add(RIVER_SAPLING.get());
+
+		this.tag(Tags.Blocks.STRIPPED_LOGS).add(STRIPPED_DRIFTWOOD_LOG.get(), STRIPPED_RIVER_LOG.get());
+		this.tag(Tags.Blocks.STRIPPED_WOODS).add(STRIPPED_DRIFTWOOD.get(), STRIPPED_RIVER_WOOD.get());
+
+		this.tag(BlockTags.REPLACEABLE).add(BEACHGRASS.get(), TALL_BEACHGRASS.get());
 
 		this.tag(UABlockTags.DRIFTWOOD_LOGS).add(DRIFTWOOD_LOG.get(), DRIFTWOOD.get(), STRIPPED_DRIFTWOOD_LOG.get(), STRIPPED_DRIFTWOOD.get());
 		this.tag(UABlockTags.RIVER_LOGS).add(RIVER_LOG.get(), RIVER_WOOD.get(), STRIPPED_RIVER_LOG.get(), STRIPPED_RIVER_WOOD.get());
@@ -74,5 +88,32 @@ public class UABlockTagsProvider extends BlockTagsProvider {
 		this.tag(BlueprintBlockTags.WOODEN_BOOKSHELVES).add(DRIFTWOOD_BOOKSHELF.get(), RIVER_BOOKSHELF.get());
 		this.tag(BlueprintBlockTags.WOODEN_CHISELED_BOOKSHELVES).add(CHISELED_DRIFTWOOD_BOOKSHELF.get(), CHISELED_RIVER_BOOKSHELF.get());
 		this.tag(BlueprintBlockTags.LEAF_PILES).add(RIVER_LEAF_PILE.get());
+
+		this.tag(Tags.Blocks.COBBLESTONES_MOSSY).add(KELPY_COBBLESTONE.get());
+
+		CoralType.values().stream().filter(coralType -> !coralType.vanilla() && coralType != CoralType.PRISMARINE).forEach(coralType -> {
+			this.tag(BlockTags.CORAL_PLANTS).add(coralType.coral().get());
+			this.tag(BlockTags.CORALS).add(coralType.fan().get());
+			this.tag(BlockTags.WALL_CORALS).add(coralType.wallFan().get());
+			this.tag(BlockTags.CORAL_BLOCKS).add(coralType.coralBlock().get());
+		});
+	}
+
+	public static Function<Block, Boolean> is(Class<? extends Block> blockClass) {
+		return blockClass::isInstance;
+	}
+
+	public void tag(TagKey<Block> tagKey, Function<Block, Boolean> condition) {
+		this.tag(tagKey, condition, null);
+	}
+
+	public void tag(TagKey<Block> tagKey, Function<Block, Boolean> condition, TagKey<Block> altTagKey, Block... alts) {
+		entries.forEach(holder -> {
+			Block block = holder.get();
+			IntrinsicTagAppender<Block> appender = this.tag(List.of(alts).contains(block) ? altTagKey : tagKey);
+			if (condition.apply(block)) {
+				appender.add(block);
+			}
+		});
 	}
 }
--- src/main/java/com/teamabnormals/upgrade_aquatic/core/data/server/tags/UAItemTagsProvider.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/data/server/tags/UAItemTagsProvider.java
@@ -7,15 +7,17 @@ import com.teamabnormals.upgrade_aquatic.core.UpgradeAquatic;
 import com.teamabnormals.upgrade_aquatic.core.other.tags.UABlockTags;
 import com.teamabnormals.upgrade_aquatic.core.other.tags.UAItemTags;
 import com.teamabnormals.upgrade_aquatic.core.registry.UABlocks;
-import com.teamabnormals.upgrade_aquatic.core.registry.UAItems;
 import net.minecraft.core.HolderLookup.Provider;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 import java.util.concurrent.CompletableFuture;
+
+import static com.teamabnormals.upgrade_aquatic.core.registry.UAItems.*;
 
 public class UAItemTagsProvider extends BlueprintItemTagsProvider {
 
@@ -29,13 +31,15 @@ public class UAItemTagsProvider extends BlueprintItemTagsProvider {
 		this.copy(UABlockTags.DRIFTWOOD_LOGS, UAItemTags.DRIFTWOOD_LOGS);
 		this.copy(UABlockTags.RIVER_LOGS, UAItemTags.RIVER_LOGS);
 
-		this.tag(ItemTags.BOATS).add(UAItems.DRIFTWOOD_BOAT.getFirst().get(), UAItems.RIVER_BOAT.getFirst().get());
-		this.tag(ItemTags.CHEST_BOATS).add(UAItems.DRIFTWOOD_BOAT.getSecond().get(), UAItems.RIVER_BOAT.getSecond().get());
-		this.tag(BlueprintItemTags.FURNACE_BOATS).add(UAItems.DRIFTWOOD_FURNACE_BOAT.get(), UAItems.RIVER_FURNACE_BOAT.get());
-		this.tag(BlueprintItemTags.LARGE_BOATS).add(UAItems.LARGE_DRIFTWOOD_BOAT.get(), UAItems.LARGE_RIVER_BOAT.get());
+		this.tag(ItemTags.BOATS).add(DRIFTWOOD_BOAT.getFirst().get(), RIVER_BOAT.getFirst().get());
+		this.tag(ItemTags.CHEST_BOATS).add(DRIFTWOOD_BOAT.getSecond().get(), RIVER_BOAT.getSecond().get());
+		this.tag(BlueprintItemTags.FURNACE_BOATS).add(DRIFTWOOD_FURNACE_BOAT.get(), RIVER_FURNACE_BOAT.get());
+		this.tag(BlueprintItemTags.LARGE_BOATS).add(LARGE_DRIFTWOOD_BOAT.get(), LARGE_RIVER_BOAT.get());
 
-		this.tag(ItemTags.TRIM_MATERIALS).add(UAItems.THRASHER_TOOTH.get());
+		this.tag(ItemTags.TRIM_MATERIALS).add(THRASHER_TOOTH.get());
 		this.tag(ItemTags.DYEABLE).add(UABlocks.BEDROLL.asItem());
+		this.tag(ItemTags.FISHES).add(PIKE.get(), COOKED_PIKE.get(), PERCH.get(), COOKED_PERCH.get(), LIONFISH.get(), COOKED_LIONFISH.get());
+		this.tag(ItemTags.DECORATED_POT_SHERDS).add(PREDATOR_POTTERY_SHERD.get());
 
 		CoralType.values().forEach(coralType -> {
 			this.tag(coralType.itemTag()).add(coralType.coral().get().asItem(), coralType.fan().get().asItem());
@@ -44,5 +48,16 @@ public class UAItemTagsProvider extends BlueprintItemTagsProvider {
 
 		this.tag(UAItemTags.PRISMARINE_CORALS).add(UABlocks.PRISMARINE_CORAL_SHOWER.get().asItem());
 		this.tag(UAItemTags.ELDER_PRISMARINE_CORALS).add(UABlocks.ELDER_PRISMARINE_CORAL_SHOWER.get().asItem());
+
+		this.copy(BlockTags.STAIRS, ItemTags.STAIRS);
+		this.copy(BlockTags.SLABS, ItemTags.SLABS);
+		this.copy(BlockTags.WALLS, ItemTags.WALLS);
+		this.copy(BlockTags.DOORS, ItemTags.DOORS);
+		this.copy(BlockTags.TRAPDOORS, ItemTags.TRAPDOORS);
+
+		this.copy(BlockTags.SMALL_FLOWERS, ItemTags.SMALL_FLOWERS);
+		this.copy(BlockTags.TALL_FLOWERS, ItemTags.TALL_FLOWERS);
+
+		this.tag(Tags.Items.MUSIC_DISCS).add(MUSIC_DISC_ATLANTIS.get());
 	}
 }
