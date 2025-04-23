0d095484311ceaccb4b21639f916c35706c9ad1d
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-04-23 09:43:13 -0500

    Update structure repaletter

--- src/main/java/com/teamabnormals/upgrade_aquatic/core/registry/datapack/UAStructureRepaletters.java
+++ src/main/java/com/teamabnormals/upgrade_aquatic/core/registry/datapack/UAStructureRepaletters.java
@@ -14,37 +14,28 @@ import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.neoforged.neoforge.common.ModConfigSpec;
-import net.neoforged.neoforge.common.conditions.ICondition;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public final class UAStructureRepaletters {
+	public static final ResourceKey<StructureRepaletterEntry> KELPY_OCEAN_RUINS = create("kelpy_ocean_ruins");
+
+	public static final ConfigValueCondition CONFIG = config(UAConfig.COMMON.kelpyOceanRuins, "kelpy_ocean_ruins");
 
 	public static void bootstrap(BootstrapContext<StructureRepaletterEntry> context) {
 		HolderGetter<Structure> structures = context.lookup(Registries.STRUCTURE);
-		ConfigValueCondition config = config(UAConfig.COMMON.kelpyOceanRuins, "kelpy_ocean_ruins");
 
-		basicRepaletter(context, structures, config, "kelpy_cobblestone_in_ocean_ruins", Blocks.MOSSY_COBBLESTONE, UABlocks.KELPY_COBBLESTONE.get(), BuiltinStructures.OCEAN_RUIN_WARM, BuiltinStructures.OCEAN_RUIN_COLD);
-		basicRepaletter(context, structures, config, "kelpy_stone_bricks_in_ocean_ruins", Blocks.MOSSY_STONE_BRICKS, UABlocks.KELPY_STONE_BRICKS.get(), BuiltinStructures.OCEAN_RUIN_WARM, BuiltinStructures.OCEAN_RUIN_COLD);
-	}
-
-	@SafeVarargs
-	private static void basicRepaletter(BootstrapContext<StructureRepaletterEntry> context, HolderGetter<Structure> structures, ICondition condition, String name, Block replacesBlock, Block replacesWith, ResourceKey<Structure>... selector) {
-		context.register(
-				repaletterKey(name),
-				new StructureRepaletterEntry(
-						// TODO: conditional holder set?
-						HolderSet.direct(Stream.of(selector).map(structures::getOrThrow).collect(Collectors.toList())),
-						Optional.empty(), false,
-						new SimpleStructureRepaletter(replacesBlock, replacesWith))
-		);
+		context.register(KELPY_OCEAN_RUINS, new StructureRepaletterEntry(HolderSet.direct(
+				structures.getOrThrow(BuiltinStructures.OCEAN_RUIN_WARM)
+		), Optional.empty(), false, 100, Optional.empty(), List.of(
+				new SimpleStructureRepaletter(Blocks.MOSSY_COBBLESTONE, UABlocks.KELPY_COBBLESTONE.get()),
+				new SimpleStructureRepaletter(Blocks.MOSSY_COBBLESTONE, UABlocks.KELPY_COBBLESTONE.get())
+		)));
 	}
 
 	public static ConfigValueCondition config(ModConfigSpec.ConfigValue<?> value, String key, boolean inverted) {
@@ -55,7 +46,7 @@ public final class UAStructureRepaletters {
 		return config(value, key, false);
 	}
 
-	private static ResourceKey<StructureRepaletterEntry> repaletterKey(String name) {
+	private static ResourceKey<StructureRepaletterEntry> create(String name) {
 		return ResourceKey.create(BlueprintDataPackRegistries.STRUCTURE_REPALETTERS, UpgradeAquatic.location(name));
 	}
 
